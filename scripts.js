09166c62df6fa156b06676ffc778d41bddc175ac
kurigomecha <rey22test@gmail.com>
2022-10-17 19:47:17 +0400

    Add all four times of day to `greet()`

--- scripts.js
+++ scripts.js
@@ -2,11 +2,15 @@ let hours = new Date().getHours();
 let greeting;
 
 function greet() { 
-  if (hours < 18) {
-    greeting = `Good day!`;
+  if (hours < 6) {
+    greeting = `Hey there, night owl!`;
+  } else if (hours < 12) {
+    greeting = `Good morning!`;
+  } else if (hours < 18) {
+    greeting = `Good afternoon!`;
   } else {
     greeting = `Good evening!`;
   }
 
-  document.querySelector(".goodTime").innerHTML = `<strong>${greeting}</strong><br><br> Current timestamp is:<br>` + Date();
+  document.querySelector(`.goodTime`).innerHTML = `<strong>${greeting}</strong><br><br> Current timestamp is:<br>` + Date();
 }
